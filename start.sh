d52a61c2b8a6038744acca4dc5f751b628affe47
Anshvachhani99 <92299607+Anshvachhani99@users.noreply.github.com>
2022-02-12 20:37:54 +0530

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
-  echo "Cloning main Repository
-  git clone https://github.com/Anshvachhani99/AnshFilterBot.git /AnshFilterBot
+  echo "Cloning main Repository"
+  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AnshFilterBot
+  git clone $UPSTREAM_REPO /EvaMaria
 fi
 cd /EvaMaria
 pip3 install -U -r requirements.txt
