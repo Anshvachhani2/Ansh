d4413762c4fea1b9a39cb48d93454d4e1a633c7f
Anshvachhani99 <92299607+Anshvachhani99@users.noreply.github.com>
2022-07-16 16:13:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -69,7 +69,7 @@ async def start(client, message):
         btn = [
             [
                 InlineKeyboardButton(
-                    "🤖 Join Updates Channel", url=invite_link.invite_link
+                    "💝 Join Channel", url=invite_link.invite_link
                 )
             ]
         ]
@@ -77,7 +77,7 @@ async def start(client, message):
         if message.command[1] != "subscribe":
             kk, file_id = message.command[1].split("_", 1)
             pre = 'checksubp' if kk == 'filep' else 'checksub' 
-            btn.append([InlineKeyboardButton(" 🔄 Try Again", callback_data=f"{pre}#{file_id}")])
+            btn.append([InlineKeyboardButton(" ✅ Refresh", callback_data=f"{pre}#{file_id}")])
         await client.send_message(
             chat_id=message.from_user.id,
             text="**Please Join My Updates Channel to use this Bot!**",
