69dbe2f934d2f24df7643ddb3cdebeaf8d9b059e
Anshvachhani99 <92299607+Anshvachhani99@users.noreply.github.com>
2022-07-17 19:41:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -79,7 +79,7 @@ async def start(client, message):
                 kk, file_id = message.command[1].split("_", 1)
                 pre = 'checksubp' if kk == 'filep' else 'checksub' 
                 btn.append([InlineKeyboardButton(" ✅ Refresh", callback_data=f"{pre}#{file_id}")])
-            except IndexError:
+            except (IndexError, ValueError):
                 btn.append([InlineKeyboardButton(" 🔄 Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
         await client.send_message(
             chat_id=message.from_user.id,
