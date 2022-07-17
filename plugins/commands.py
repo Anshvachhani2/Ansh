49ef9b05487c49bf8b9b06160d02de55a610ceb6
Anshvachhani99 <92299607+Anshvachhani99@users.noreply.github.com>
2022-07-17 19:44:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -80,7 +80,7 @@ async def start(client, message):
                 pre = 'checksubp' if kk == 'filep' else 'checksub' 
                 btn.append([InlineKeyboardButton(" ✅ Refresh", callback_data=f"{pre}#{file_id}")])
             except (IndexError, ValueError):
-                btn.append([InlineKeyboardButton(" 🔄 Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
+                btn.append([InlineKeyboardButton(" ✅ Refresh", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
         await client.send_message(
             chat_id=message.from_user.id,
             text="**🌀 Aapne Hamare channel ko join nhin kiya hai join kare phle or refresh pe click kare bot work 🌀.**\n\n**💫 Step 1 join channel 💜**\n**💫 Step 2 refresh and bot working...**",
